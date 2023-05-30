42104095daef26d28af4a5cc4891544dc0d0e6de
puchochiri <doawishfor@naver.com>
2023-05-30 22:50:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jarproject_ex
+폐쇄망용,  jar포함
---
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,39 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages= {"org.zerock.service"})
+@ComponentScan(basePackages= {"org.zerock.sample"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mariadb://localhost:3306/book_ex");
+		
+		hikariConfig.setUsername("book_ex");
+		// 회사
+//		hikariConfig.setPassword("pucho8934!");
+		// 집
+		hikariConfig.setPassword("book_ex");
+//		hikariConfig.setUsername("book_ex");
+//		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	
+	
+
+}
